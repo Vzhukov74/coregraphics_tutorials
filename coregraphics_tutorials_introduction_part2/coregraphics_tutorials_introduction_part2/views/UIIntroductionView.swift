442da88fd9ab608750217e3e3f966689126fbca2
vladislav zhukov <vzhukov@protonmail.ch>
2019-03-19 14:51:39 +0500

    did init project for part 2

---
+++ coregraphics_tutorials_introduction_part2/coregraphics_tutorials_introduction_part2/views/UIIntroductionView.swift
@@ -0,0 +1,12 @@
+//
+//  UIIntroductionView.swift
+//  coregraphics_tutorials_introduction_part2
+
+import UIKit
+
+class UIIntroductionView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+}
