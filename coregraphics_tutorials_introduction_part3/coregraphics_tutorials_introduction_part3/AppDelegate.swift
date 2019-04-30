4cbc6f47250393645d5ea70534ae1dc5ed181414
vladislav zhukov <vzhukov@protonmail.ch>
2019-04-30 16:25:56 +0500

    added part3

---
+++ coregraphics_tutorials_introduction_part3/coregraphics_tutorials_introduction_part3/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  coregraphics_tutorials_introduction_part3
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ coregraphics_tutorials_introduction_part3/coregraphics_tutorials_introduction_part3/views/UIIntroductionView.swift
@@ -0,0 +1,10 @@
+//
+//  UIIntroductionView.swift
+//  coregraphics_tutorials_introduction_part3
+
+import UIKit
+
+@IBDesignable
+class UIIntroductionView: UIView {
+
+}
